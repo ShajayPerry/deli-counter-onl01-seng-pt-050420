a64272447ec677def2732a1f36cf3ff7b9789965
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-16 15:29:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,4 +12,11 @@ def line(array)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end
     
